aeafe4de7f2d42bba94be43e399e8294313ba82c
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2023-08-12 02:25:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+> Alacritty setup
+
+Preferred path `~/.config/alacritty`
+
+```
+chmod u+x init.sh
+./init.sh
+```
+
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+git clone https://github.com/EdenEast/nightfox.nvim ~/.config/alacritty/nightfox.nvim
+
